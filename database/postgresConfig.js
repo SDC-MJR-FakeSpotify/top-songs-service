519289c7fc08272b7f5c767e7cef3f8784836007
Joseph Montoya <jtmontoya26@gmail.com>
2020-07-21 08:20:39 -0700

    Updated http get routes due to restart of AWS EC2 instances

--- database/postgresConfig.js
+++ database/postgresConfig.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes, Models } = require('sequelize');
 const sequelize = new Sequelize('spotify', 'postgres', 'password', {
-  host: 'ec2-13-52-182-205.us-west-1.compute.amazonaws.com',
+  host: 'ec2-54-153-116-222.us-west-1.compute.amazonaws.com',
   dialect: 'postgres',
 })
 
